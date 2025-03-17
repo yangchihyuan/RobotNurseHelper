87156bb6000646333a54e931f2db74b26a4d8ccb
Chih-Yuan Yang <cyyang@cgu.edu.tw>
2025-03-17 11:56:01 +0800

    Tablet app sends message received by the Server-side program

---
+++ Tablet/app/src/main/java/com/example/painrating/NetworkSettingActivity.java
@@ -0,0 +1,51 @@
+package com.example.painrating;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NetworkSettingActivity extends AppCompatActivity {
+
+    private ImageButton btnGoPain, btnGoVideo;
+    private Button btnNetworkSetting;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGoPain = findViewById(R.id.btnGoPain);
+        btnGoVideo = findViewById(R.id.btnGoVideo);
+        btnNetworkSetting = findViewById(R.id.btnNetworkSetting);
+
+        // 1. 點擊 -> PainActivity
+        btnGoPain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NetworkSettingActivity.this, PainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 2. 點擊 -> VideoActivity
+        btnGoVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NetworkSettingActivity.this, VideoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnNetworkSetting.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NetworkSettingActivity.this, NetworkSettingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
