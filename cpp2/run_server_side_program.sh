aeb7e354fc94d2e943ce90b50de37da737c62d7a
Chih-Yuan Yang <cyyang@cgu.edu.tw>
2025-04-19 04:02:27 +0800

    update install.sh and run_server_side_program.sh

--- cpp2/run_server_side_program.sh
+++ cpp2/run_server_side_program.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #Author: Chih-Yuan Yang
-#2025/3/12
+#2025/3/18
 
 if [ $# == 0 ]; then
     build/ZenboNurseHelper -m $HOME/whisper.cpp/models/ggml-base.bin -d $HOME/Downloads
+elif [ $1 == "4070" ]; then
+    build/ZenboNurseHelper -m $HOME/whisper.cpp/models/large-v3-turbo -d $HOME/Downloads
 elif [ $1 == "debug" ]; then
     gdb --args build/ZenboNurseHelper "-m" "$HOME/whisper.cpp/models/ggml-base.bin" "-d" "$HOME/Downloads"
 elif [ $1 == "valgrind" ]; then
